08675c4e618df844dda63b3030f3d23a024726d7
prateeeksahu <100373713+prateeeksahu@users.noreply.github.com>
2024-01-28 22:25:35 +0530

    280124

--- javaOops/oops/src/staticKeword.java
+++ javaOops/oops/src/staticKeword.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class staticKeword {
+public class staticKeword {
+    // class Test  // ERROR
+    static class Test{
+        String name;
+
+        public Test(String name) {
+            this.name = name;
+        }
+    }
+
+    public static void main(String[] args) {
+        Test a = new Test("prateek");
+
+        Test b = new Test("anurag");
+
+        System.out.println(a.name); // prateek
+        System.out.println(b.name); // anurag
+    }
 }
